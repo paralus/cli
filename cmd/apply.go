08cb6619c83a907e91881e434a1d4774c7fbc188
rustiever <sharanrdude@gmail.com>
2022-10-27 20:57:24 +0530

    update

--- cmd/apply.go
+++ cmd/apply.go
@@ -16,7 +16,7 @@ func newApplyCmd(applyOption commands.CmdOptions) *cobra.Command {
 		Example: `
   Using config file:
     pctl apply -f cluster-config.yml 
-    pctl apply
+    pctl apply -f cluster-config.yml
 `,
 		Args: applyOption.Validate,
 		RunE: applyOption.Run,
